eb2691e93df964c943aad67bff2bf5dfcf955b18
Degan Gabriel Ortiz Gomez <DGORTIZ@correo.url.edu.gt>
2026-03-18 18:31:11 -0600

    E01 Smallets
    retornal el valor mas pequeño

--- src/main/java/ed/lab/E01KthSmallest.java
+++ src/main/java/ed/lab/E01KthSmallest.java
@@ -2,8 +2,28 @@ package ed.lab;
 
 public class E01KthSmallest {
 
+    private int contador = 0;
+    private int resultado = 0;
+
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        inorder(root, k);
+        return resultado;
     }
 
+    private void inorder(TreeNode<Integer> nodo, int k) {
+        if (nodo == null) return;
+
+        // izquierda
+        inorder(nodo.left, k);
+
+        // visitar nodo
+        contador++;
+        if (contador == k) {
+            resultado = nodo.value;
+            return;
+        }
+
+        // derecha
+        inorder(nodo.right, k);
+    }
 }
